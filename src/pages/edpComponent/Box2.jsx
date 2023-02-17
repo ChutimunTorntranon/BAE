fe027640ae144c93eaabf8e77d69ff381833c54e
ChutimunTorntranon <ChutimunTorntranon@gmail.com>
2023-02-17 13:49:31 +0700

    chromium

--- src/pages/edpComponent/Box2.jsx
+++ src/pages/edpComponent/Box2.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import ModalImage from "react-modal-image";
 import "../lineComponent/LineStyle.css";
+import { useTranslation } from "react-i18next";
 function Box2() {
+  const { t, i18n } = useTranslation();
+  const handleChangeLng = (lng) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem("lng", lng);
+  };
+
   return (
     <div className="Box1 xl:mt-[20px] mt-[-1px]">
       <div className="container mx-auto">
         <div className="p-3 Font1 font-bold xl:text-[30px] xl:text-left text-[23px] text-center">
-          <strong>EDP คืออะไร? ดีกว่าสีพื้นทั่วไปอย่างไร?</strong>
+          <strong>{t("edp1")}</strong>
         </div>
         <ul className="list-disc p-5">
           <li className="p-3 Font2 xl:text-[21px] xl:text-left text-[15px] text-left">
-            <strong>EDP ย่อมาจาก Electro Deposition Painting</strong> คือ
-            เทคโนโลยีการชุบโลหะ ที่อาศัยกลไกทางไฟฟ้า
-            วิธีนี้จะได้ผิวเคลือบที่เรียบ สามารถทนต่อการกัดกร่อนได้สูง
-            ได้ความหนาของผิวเคลือบและเฉดสีที่สม่ำเสมอกัน
-            เพิ่มแรงยึดเกาะให้ดีขึ้น นิยมใช้สำหรับการเคลือบวัสดุที่มีความซับซ้อน
-            เช่น ชิ้นส่วนรถยนต์ ชิ้นส่วนอิเล็กทรอนิกส์ ฯลฯ
-            ชุบโลหะด้วยไฟฟ้าสามารถชุบวัสดุได้หลากหลาย ทั้งเหล็ก อลูมิเนียม ซิงค์
-            ฯลฯ
+            <strong>{t("edp2")}</strong>
+            &nbsp;{t("edp3")}
           </li>
           <li className="p-3 Font2 xl:text-[21px] xl:text-left text-[15px] text-left">
-            <strong>ข้อดีของการชุบ EDP ?</strong>
-            <br></br>1. สามารถชุบผิวโลหะเรียบความหนาสม่ำเสมอได้ เคลือบได้ทุกส่วน
-            ทั้งร่อง หรือซอกลึกที่ปกติ สเปรย์ฉีดได้ไม่ทั่วถึง<br></br>2. สามารถ
-            ป้องกันสนิมได้อย่างมีประสิทธิภาพ
-            <br></br>3. ไม่มีผลกระทบต่อสิ่งแวดล้อม
+            <strong>{t("edp4")}</strong>
+            <br></br>
+            {t("edp5")}
+            <br></br>
+            {t("edp6")}
+            <br></br>
+            {t("edp7")}
           </li>
         </ul>
       </div>
