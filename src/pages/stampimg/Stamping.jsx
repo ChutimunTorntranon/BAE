50c878e66185d60544cd0fa2839a767c8660ca33
Chutimun Torntranon <jeffymonday@Chutimuns-MacBook-Pro.local>
2023-02-19 14:26:57 +0700

    lngh

--- src/pages/stampimg/Stamping.jsx
+++ src/pages/stampimg/Stamping.jsx
@@ -1,22 +1,30 @@
-import React from "react";
-import GoToTop from "../../component/GoToTop";
-import Footer from "../../footer/Footer";
-import Box2 from "./Box2";
+import React from 'react';
+import GoToTop from '../../component/GoToTop';
+import Footer from '../../footer/Footer';
+import Box2 from './Box2';
+import { useTranslation } from 'react-i18next';
+
 function Stamping() {
-  return (
-    <div className="">
-      <div className="w-full xl:h-[200px] h-[120px] bg-indigo-600">
-        <div className="container grid mx-auto h-full">
-          <div className="mx-auto my-auto Zince text-center xl:text-[50px] text-[22px]">
-            Stamping & welding
-          </div>
-        </div>
-        <Box2 />
-        <Footer />
-        <GoToTop />
-      </div>
-    </div>
-  );
+	const { t, i18n } = useTranslation();
+	const handleChangeLng = (lng) => {
+		i18n.changeLanguage(lng);
+		localStorage.setItem('lng', lng);
+	};
+
+	return (
+		<div className=''>
+			<div className='w-full xl:h-[200px] h-[120px] bg-indigo-600'>
+				<div className='container grid mx-auto h-full'>
+					<div className='mx-auto my-auto Zince text-center xl:text-[50px] text-[22px]'>
+						{t('st')}
+					</div>
+				</div>
+				<Box2 />
+				<Footer />
+				<GoToTop />
+			</div>
+		</div>
+	);
 }
 
 export default Stamping;
--- src/pages/zincePage/ZinceProduct.jsx
+++ src/pages/zincePage/ZinceProduct.jsx
@@ -1,21 +1,29 @@
-import Box1 from "./zinceComponent/Box1";
-import Footer from "../../footer/Footer";
-import GoToTop from "../../component/GoToTop";
+import Box1 from './zinceComponent/Box1';
+import Footer from '../../footer/Footer';
+import GoToTop from '../../component/GoToTop';
+import { useTranslation } from 'react-i18next';
+
 function ZinceProduct() {
-  return (
-    <div className="">
-      <div className="xl:w-full xl:h-[200px] w-full h-[120px] bg-indigo-600">
-        <div className="container grid mx-auto h-full">
-          <div className="mx-auto my-auto Zince xl:text-[50px] text-[22px]">
-            Zinc
-          </div>
-        </div>
-        <Box1 />
-        <Footer />
-        <GoToTop />
-      </div>
-    </div>
-  );
+	const { t, i18n } = useTranslation();
+	const handleChangeLng = (lng) => {
+		i18n.changeLanguage(lng);
+		localStorage.setItem('lng', lng);
+	};
+
+	return (
+		<div className=''>
+			<div className='xl:w-full xl:h-[200px] w-full h-[120px] bg-indigo-600'>
+				<div className='container grid mx-auto h-full'>
+					<div className='mx-auto my-auto Zince xl:text-[50px] text-[22px]'>
+						{t('z1')}
+					</div>
+				</div>
+				<Box1 />
+				<Footer />
+				<GoToTop />
+			</div>
+		</div>
+	);
 }
 
 export default ZinceProduct;
